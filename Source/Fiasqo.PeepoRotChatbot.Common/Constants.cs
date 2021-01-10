b52e448baf983f9cc1a72cd47e8a7c5ad0d33cc3
Artyom Solobay <43761130+Fiasqo@users.noreply.github.com>
2021-01-10 04:58:28 +0300

    Remove unused fields and change the 'MinMainWindowHeight' value

--- Source/Fiasqo.PeepoRotChatbot.Common/Constants.cs
+++ Source/Fiasqo.PeepoRotChatbot.Common/Constants.cs
@@ -6,16 +6,13 @@ namespace Fiasqo.PeepoRotChatbot.Common {
 public static class Constants {
 	public const string NotAvailableMessage = "<N/A>";
 
-	public const double MinMainWindowWidth = 1024.0D;
-	public const double MinMainWindowHeight = 576.0D;
+	public const double MinMainWindowWidth = 1024D;
+	public const double MinMainWindowHeight = 613D;
 
 	public const int MaxChatMessageLenght = 500;
 	public const int MaxStreamTitleLenght = 140;
 	public const int MaxUserNameLenght = 25;
 	public const int MaxChatCommandLenght = 50;
-
-	public const int TwoYears_Seconds = 63070000;
-	public const int OneMonth_Seconds = 2592000;
 
 	public const int MinGapBetweenTimerReplies_Seconds = 600;
 	public const int MinGapBetweenEventsReplies_Seconds = 1;
